9240be4b4dc48c7055daafa8d5ae8981428e56bc
Federico Berra <fede.berra@hotmail.it>
2018-12-12 17:57:59 +0100

    Prima visualizzazione corretta del grafico

--- crosscorrelation.m
+++ crosscorrelation.m
@@ -1,4 +1,4 @@
-function [lagDiff] = crosscorrelation(s1, s2, windowSize)
+function [lagDiff] = crosscorrelation(s2, s1, windowSize)
     lagDiff = [];
     for i = 1:ceil(size(s1/windowSize, 1))
         vMin = (i-1) * windowSize + 1;
@@ -6,7 +6,7 @@ function [lagDiff] = crosscorrelation(s1, s2, windowSize)
         if vMax > size(s1, 1)
             vMax = size(s1, 1); 
         end
-        [acor, lag] = xcorr(s1(vMin:vMax), s2(vMin:vMax));
+        [acor, lag] = xcorr(abs(fft(s1(vMin:vMax))), abs(fft(s2(vMin:vMax))));
         [~,I] = max(abs(acor));
         lagDiff = [lagDiff lag(I)];
     end
--- mainLab1.m
+++ mainLab1.m
@@ -4,23 +4,38 @@ close all
 clearvars
 
 %######################_CONST_#######################
-maxFileNumber = 3;
-windowSize = 10000;
+maxFileNumber = 2;
+windowSize = 5000;
+signalStart = 6.7; 
+signalEnd = 12.3;
+vectorStart= 0;
+vectorEnd = 0;
 
 %#######################_MAIN_#######################
+% Calcolo della lunghezza della fibra
+[z, P, S, info] = loadOBR("dataLab1/1.obr");
+for i = 1:size(z, 2)
+    if(z(i) <= signalStart)
+        vectorStart = i+1;
+    end
+    if(z(i) <= signalEnd)
+        vectorEnd = i;
+    end
+end
+
 % Importazione
 % dati(i).meters(1) sono tutti uguali
 dati = struct('meters',[],'polarizeP',[],'polarizeS',[],'info',{});
 for i = 1:maxFileNumber
     [z, P, S, info] = loadOBR("dataLab1/"+i+".obr");
-    dati(i).meters = z;
-    dati(i).polarizeP = P;
-    dati(i).polarizeS = S;
+    dati(i).meters = z(vectorStart:vectorEnd);
+    dati(i).polarizeP = P(vectorStart:vectorEnd);
+    dati(i).polarizeS = S(vectorStart:vectorEnd);
     dati(i).info = info;
 end
 
 % Cross correlazioni
-arrayShift = zeros(maxFileNumber, ceil(size(dati(1).polarizeP, 1)/windowSize));
+arrayShift = zeros(maxFileNumber, ceil(size(dati(1).polarizeP, 1) / windowSize));
 for i = 1:maxFileNumber
     shift = crosscorrelation(dati(1).polarizeP, dati(i).polarizeP, windowSize);
     for j = 1:size(shift, 2)
@@ -33,3 +48,13 @@ end
 xAxis = 1 : size(arrayShift, 2);
 plot(xAxis, arrayShift);
 % hold off
+
+% hold on;
+% title('|A|, |Z|, |HBPF|');
+% plot(f, mag2db(abs(A)));
+% plot(f, mag2db(abs(Z)));
+% plot(f, mag2db(abs(HBPF)));
+% ylim([-45 5]);
+% legend('|A|', '|Z|', '|HBPF|');
+% xlabel('frequenza[Hz]');
+% hold off;
